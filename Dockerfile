a86e07cdfdd05eaf7a1cbc77347678a31de9d2b7
Asilbek <147981875+alijonovasilbek@users.noreply.github.com>
2024-08-27 21:42:29 +0500

    updated

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+#bu mening dockerfilem
+
+FROM debian:buster-slim
+
+RUN apt-get update \
+ && apt-get install -y nginx
+
+
+COPY ./index1.html /var/www/html
+
+# CMD nginx -g 'daemon off;'
+CMD [ "nginx","-g","daemon off;" ]
--- migrations/versions/4c6473b6e903_enum_column_added.py
+++ migrations/versions/4c6473b6e903_enum_column_added.py
@@ -1,8 +1,8 @@
-"""initial migration
+""" enum column added 
 
-Revision ID: 461fc41bbbe1
+Revision ID: 4c6473b6e903
 Revises: 
-Create Date: 2024-08-20 19:14:05.190979
+Create Date: 2024-08-27 10:44:06.680002
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '461fc41bbbe1'
+revision: str = '4c6473b6e903'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -30,6 +30,16 @@ def upgrade() -> None:
     sa.Column('tag_name', sa.String(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_table('occupation',
+    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('occup_name', sa.String(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('skills',
+    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('skill_name', sa.String(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('user',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('first_name', sa.String(), nullable=True),
@@ -41,6 +51,8 @@ def upgrade() -> None:
     sa.Column('is_seller', sa.Boolean(), nullable=True),
     sa.Column('is_client', sa.Boolean(), nullable=True),
     sa.Column('is_superuser', sa.Boolean(), nullable=True),
+    sa.Column('telegram_username', sa.String(), nullable=True),
+    sa.Column('phone_number', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
@@ -53,8 +65,10 @@ def upgrade() -> None:
     sa.Column('status', sa.Boolean(), nullable=False),
     sa.Column('category_id', sa.Integer(), nullable=True),
     sa.Column('user_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['category_id'], ['gigs_category.id'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+    sa.Column('job_type', sa.Enum('full_time', 'part_time', 'contract', 'one_time_project', 'internship', name='jobtypeenum'), nullable=False),
+    sa.Column('work_mode', sa.Enum('online', 'offline', name='workmodeenum'), nullable=False),
+    sa.ForeignKeyConstraint(['category_id'], ['gigs_category.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('seller',
@@ -64,14 +78,14 @@ def upgrade() -> None:
     sa.Column('description', sa.Text(), nullable=True),
     sa.Column('cv_url', sa.Text(), nullable=True),
     sa.Column('birth_date', sa.Date(), nullable=True),
-    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('certificate',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('pdf_url', sa.Text(), nullable=True),
     sa.Column('seller_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('experience',
@@ -84,7 +98,7 @@ def upgrade() -> None:
     sa.Column('country', sa.String(), nullable=True),
     sa.Column('job_title', sa.String(), nullable=True),
     sa.Column('description', sa.Text(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('gig_tag_connect',
@@ -101,67 +115,52 @@ def upgrade() -> None:
     sa.ForeignKeyConstraint(['gigs_id'], ['gigs.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
-    op.create_table('language',
-    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('lan_name', sa.String(), nullable=False),
-    sa.Column('seller_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
-    op.create_table('occupation',
-    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('occup_name', sa.String(), nullable=False),
-    sa.Column('seller_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
     op.create_table('saved_client',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('seller_id', sa.Integer(), nullable=True),
     sa.Column('user_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('saved_seller',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('seller_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('seller_occupation',
+    sa.Column('seller_id', sa.Integer(), nullable=False),
+    sa.Column('occupation_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['occupation_id'], ['occupation.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('seller_id', 'occupation_id')
     )
     op.create_table('seller_projects',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('title', sa.String(), nullable=False),
-    sa.Column('category', sa.String(), nullable=True),
     sa.Column('price', sa.Float(), nullable=False),
     sa.Column('delivery_days', sa.Integer(), nullable=True),
     sa.Column('seller_id', sa.Integer(), nullable=True),
     sa.Column('description', sa.Text(), nullable=True),
-    sa.Column('status', sa.String(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
+    sa.Column('status', sa.Boolean(), nullable=False),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
-    op.create_table('skills',
-    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('skill_name', sa.String(), nullable=False),
-    sa.Column('seller_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ),
-    sa.PrimaryKeyConstraint('id')
+    op.create_table('seller_skills',
+    sa.Column('seller_id', sa.Integer(), nullable=False),
+    sa.Column('skill_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['seller_id'], ['seller.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['skill_id'], ['skills.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('seller_id', 'skill_id')
     )
     op.create_table('project_files',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('file_url', sa.Text(), nullable=True),
     sa.Column('seller_project_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_project_id'], ['seller_projects.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
-    op.create_table('projects_skills',
-    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('skill_name', sa.String(), nullable=False),
-    sa.Column('seller_project_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['seller_project_id'], ['seller_projects.id'], ),
+    sa.ForeignKeyConstraint(['seller_project_id'], ['seller_projects.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
@@ -169,14 +168,12 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     # ### commands auto generated by Alembic - please adjust! ###
-    op.drop_table('projects_skills')
     op.drop_table('project_files')
-    op.drop_table('skills')
+    op.drop_table('seller_skills')
     op.drop_table('seller_projects')
+    op.drop_table('seller_occupation')
     op.drop_table('saved_seller')
     op.drop_table('saved_client')
-    op.drop_table('occupation')
-    op.drop_table('language')
     op.drop_table('gigs_file')
     op.drop_table('gig_tag_connect')
     op.drop_table('experience')
@@ -184,6 +181,8 @@ def downgrade() -> None:
     op.drop_table('seller')
     op.drop_table('gigs')
     op.drop_table('user')
+    op.drop_table('skills')
+    op.drop_table('occupation')
     op.drop_table('gigs_tags')
     op.drop_table('gigs_category')
     # ### end Alembic commands ###
